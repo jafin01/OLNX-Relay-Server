69c9b00752556ea0d4f6525d9efba1a828f6e7f0
Jafin Jahfar <jafinjahfar@gmail.com>
2023-06-19 15:55:28 +0530

    voice id recieving from request update

--- controller/textToSpeech.js
+++ controller/textToSpeech.js
@@ -3,13 +3,11 @@ import axios from "axios";
 export async function textToSpeech (req, res) {
   try {
     const apiKey = process.env.ELEVENLABS_API_KEY;
-    const voiceId = process.env.ELEVENLABS_VOICE_ID;
+    const voiceId = req.body.voice_id;
     const text = req.body.text;
     const model_id = req.body.model_id;
     const voice_settings = req.body.voice_settings;
     // const fineTuning = req.body.fineTuning;
-
-    console.log(text);
 
     const {data} = await axios.post(
       `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`,
